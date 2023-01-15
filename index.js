387f28f3748c170b9273b73ee95f687caa585e72
mikej321 <111183140+mikej321@users.noreply.github.com>
2023-01-15 14:32:53 -0500

    update 1

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-let lastX;
-let bar = document.querySelector('div');
-
-bar.addEventListener('mousedown', event => {
-  if (event.button == 0) {
-    lastX = event.clientX;
-    window.addEventListener('mousemove', moved);
-    event.preventDefault();
+let update = (event) => {
+  for (let dot; dot = document.querySelector('dot');) {
+    dot.remove();
   }
-});
-
-let moved = (event) => {
-  if (event.buttons == 0) {
-    window.removeEventListener('mousemove', moved);
-  } else {
-    let dist = event.clientX - lastX;
-    let newWidth = Math.max(10, bar.offsetWidth + dist);
-    bar.style.width = newWidth + 'px';
-    lastX = event.clientX;
+  for (let i = 0; i < event.touches.length; i++) {
+    let {pageX, pageY} = event.touches[i];
+    let dot = document.createElement('dot');
+    dot.style.left = (pageX - 50) + 'px';
+    dot.style.top = (pageY - 50) + 'px';
+    document.body.appendChild(dot);
   }
 }
+
+window.addEventListener('touchstart', update);
+window.addEventListener('touchmove', update);
+window.addEventListener('touchend', update);
